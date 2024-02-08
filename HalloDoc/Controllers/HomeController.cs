cc572b81bcb01cdac03334623d5122b6b89a4bbb
Sahil-Jarsaniya <sahil.jarsaniya@etatvasoft.com>
2024-02-08 18:26:43 +0530

    login and dashboard

--- HalloDoc/Controllers/HomeController.cs
+++ HalloDoc/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Diagnostics;
+using HalloDoc.DataAccess.Data;
 using HalloDoc.DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace HalloDoc.Controllers;
-
+using Microsoft.AspNetCore.Http;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
     {
         _logger = logger;
+        _db = db;
+
     }
 
     public IActionResult Index()
@@ -22,10 +26,49 @@ public class HomeController : Controller
     {
         return View();
     }
+
     public IActionResult login()
     {
         return View();
     }
+    [HttpPost]
+    public IActionResult login(AspNetUser user)
+    {
+        var myUser = _db.AspNetUsers.Where(x => x.UserName == user.UserName && x.PasswordHash == user.PasswordHash).FirstOrDefault();
+        if (myUser == null)
+        {
+            ViewBag.message = "Login Failed";
+            return View();
+        }
+        else
+        {
+            HttpContext.Session.SetString("token", user.UserName);
+            return RedirectToAction("dashboard", "Home");
+        }
+    }
+    public IActionResult logout()
+    {
+        if (HttpContext.Session.GetString("token") != null)
+        {
+            HttpContext.Session.Remove("token");
+            return RedirectToAction("login");
+        }
+        return View();
+    }
+    public IActionResult dashboard()
+    {
+        if (HttpContext.Session.GetString("token") != null)
+        {
+            ViewBag.Data = HttpContext.Session.GetString("token").ToString();
+        }
+        else
+        {
+            return RedirectToAction("login");
+        }
+
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- HalloDoc/Program.cs
+++ HalloDoc/Program.cs
@@ -1,14 +1,19 @@
+using HalloDoc.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//{
-//    options.UseNpgsql(
-//        builder.Configuration.GetConnectionString("DefaultConnetion")
-//        );
-//});
+builder.Services.AddSession();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnetion")
+        );
+});
 
 var app = builder.Build();
 
@@ -19,7 +24,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
